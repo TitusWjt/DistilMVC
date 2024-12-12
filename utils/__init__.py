a376dcb83b98d615539fc0a44f6d4882410f19f2
Titus <87708666+TitusWjt@users.noreply.github.com>
2024-12-12 21:23:23 +0800

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .yaml_config_hook import yaml_config_hook
-from .dict_split import dict_slice
-from .dict_split import exact_weights
-from .create_dict import create_dict
-from .create_dict import recode
-from .log import get_logger
